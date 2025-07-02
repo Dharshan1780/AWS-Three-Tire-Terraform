1acff27c01f115c415e9a9cfb81b3cd7b3dbf4b5
Dharshan <147246021+Dharshan1780@users.noreply.github.com>
2025-07-02 14:07:18 +0530

    Update output.tf

--- Three-tire arck/output.tf
+++ Three-tire arck/output.tf
@@ -1,7 +1,6 @@
 output "load_balancer_endpoint" {
   value = module.loadbalancer.web_alb_endpoint
 }
-
-# output "database_endpoint" {
-#   value = module.db.rds_db_endpoint
-# }
+output "database_endpoint" {
+  value = module.db.rds_db_endpoint
+}
